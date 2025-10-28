dcad1e704a624b8060013d346a41d8a7c5ab4be3
Sumant <sumantsingh236@gmail.com>
2025-10-28 16:01:12 +0530

    setting environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,8 +9,8 @@ import {
 const firebaseConfig = {
   // Config your firebase here
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "busybuy-45dda.firebaseapp.com",
-  projectId: "busybuy-45dda",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: "busybuy-45dda.firebasestorage.app",
   messagingSenderId: "512585049947",
   appId: "1:512585049947:web:cbf69b4d738cb0a628dc7e",
